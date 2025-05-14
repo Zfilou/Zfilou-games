0705d08b4854a106e3512d84f1471fed753ec930
Zfilou <160928542+Zfilou@users.noreply.github.com>
2025-05-14 18:13:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+document.getElementById('playNow').addEventListener('click', function() {
+    window.location.href = 'games.html';  // Redirection vers la page des jeux
+});
+
+document.getElementById('topGames').addEventListener('click', function() {
+    alert('Check out the top games!');
+});
+
+document.getElementById('aboutUs').addEventListener('click', function() {
+    alert('Learn more about us!');
+});
